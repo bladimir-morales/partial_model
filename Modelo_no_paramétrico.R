78a77724c1e197962b380b9a67abe4dd8ed42eb4
Bladimir Morales Torrez <bladimir.moralest@gmail.com>
2022-08-01 00:14:17 -0400

    act

---
+++ Modelo_no_paramétrico.R
@@ -0,0 +1,37 @@
+set.seed(01012022)
+m=function(x1,x2) sin(2*pi*x1)+x2
+x1=seq(0,1,0.02)
+x2=seq(0,1,0.02)
+z=outer(x1,x2,m)
+
+error=rnorm(51,0,1/4)
+y=m(x1,x2)+error
+
+
+mod1=loess(y~x1+x2,span=0.3,degree = 0)
+z1=expand.grid(x1,x2)
+
+mod2=loess(y~x1+x2,span=0.3,degree = 1)
+z2=expand.grid(x1,x2)
+
+
+par(mfrow=c(1,3))
+persp(x1,x2,z,phi=30,col = "green",main="Función",ticktype = "detailed")
+persp(x1,x2,predict(mod1,z1),phi=30,col = "green",main="Nadaraya Watson",ticktype = "detailed")
+persp(x1,x2,predict(mod2,z2),phi=30,col = "green",main="Lineal local",ticktype = "detailed")
+
+
+
+mod3=loess(y~x1+x2,degree = 0)
+z3=expand.grid(x1,x2)
+
+mod4=loess(y~x1+x2,degree = 1)
+z4=expand.grid(x1,x2)
+
+par(mfrow=c(1,3))
+persp(x1,x2,z,phi=30,col = "green",main="Función",ticktype = "detailed")
+persp(x1,x2,predict(mod3,z3),phi=30,col = "green",main="Nadaraya Watson",ticktype = "detailed")
+persp(x1,x2,predict(mod4,z4),phi=30,col = "green",main="Lineal local",ticktype = "detailed")
+
+
+
